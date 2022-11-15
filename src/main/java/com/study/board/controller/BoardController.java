b9c591f20ea7a355293c2b73b8476e5eb3e6e24b
uuuuuuuk <s22034@gsm.hs.kr>
2022-11-15 09:54:35 +0900

    setting

---
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -0,0 +1,10 @@
+package com.study.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BoardController {
+
+}
